7c81ddf4de25681c549353c7bc4a972e44fe987a
LorenzKa <55577103+LorenzKa@users.noreply.github.com>
2025-09-03 11:08:18 +0200

    Enable compile option for faster training
    Changed compile setting to True for training optimization.

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ class Config():
         # Faster-Training settings
         self.mixed_precision = ['no', 'fp16', 'bf16', 'fp8'][1]
         self.load_all = False and self.dynamic_size is None   # Turn it on/off by your case. It may consume a lot of CPU memory. And for multi-GPU (N), it would cost N times the CPU memory to load the data.
-        self.compile = False                             # 1. Trigger CPU memory leak in some extend, which is an inherent problem of PyTorch.
+        self.compile = True                             # 1. Trigger CPU memory leak in some extend, which is an inherent problem of PyTorch.
                                                         #   Machines with > 70GB CPU memory can run the whole training on DIS5K with default setting.
                                                         # 2. Higher PyTorch version may fix it: https://github.com/pytorch/pytorch/issues/119607.
                                                         # 3. But compile in 2.0.1 < Pytorch < 2.5.0 seems to bring no acceleration for training.
